2780d62b8dac61a880a674b2461a7bfbc615e770
VinaySohaliya <Sohaliyavin01@gmail.com>
2023-09-07 14:44:05 +0530

    home page fuction are addes

--- src/layout/HomeLayout.jsx
+++ src/layout/HomeLayout.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import { FiMenu } from 'react-icons/fi'
 import { AiFillCloseCircle } from 'react-icons/ai'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import Footer from '../components/Footer';
+import { useDispatch, useSelector } from 'react-redux';
 
 
 
 const HomeLayput = ({ children }) => {
-    
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const islogin = useSelector((state) => state.auth.isLogin);
+    console.log(islogin);
+
+    const role = useSelector((state) => state.auth.role)
+
     function changeWidth() {
         const menu = document.getElementsByClassName('drawer-side');
         menu[0].style.width = 'auto';
@@ -23,6 +32,15 @@ const HomeLayput = ({ children }) => {
         // changeWidth()
     }
 
+    function handellogout(e) {
+        e.preventDefault();
+
+        if (res) {
+            
+        }
+        navigate("/")
+    }
+
     return (
         <div className=' min-h-[90vh] '>
             <div className="drawer absolute left-0 z-50 w-fit">
@@ -35,12 +53,17 @@ const HomeLayput = ({ children }) => {
                     <ul className="menu p-4 w-48 min-h-full bg-base-200  relative text-base-content">
                         <li className='w-fit absolute right-2 z-50'>
                             <button className=' cursor-pointer' onClick={hideDrawer}>
-                                <AiFillCloseCircle size={24}  />
+                                <AiFillCloseCircle size={24} />
                             </button>
                         </li>
                         <li>
                             <Link to='/'>Home</Link>
                         </li>
+                        {islogin && role == 'ADMIN' && (
+                            <li>
+                                <Link to='/admin/dashboard'>Admin DashBoard</Link>
+                            </li>
+                        )}
                         <li>
                             <Link to='/coures'>All Coureses</Link>
                         </li>
@@ -50,13 +73,42 @@ const HomeLayput = ({ children }) => {
                         <li>
                             <Link to='/aboutus'>About Us</Link>
                         </li>
+                        {!islogin &&
+                            (
+                                <li className=' absolute bottom-4 w-[90%]'>
+                                    <div className=' w-full flex items-center justify-center'>
+                                        <button className=' btn-primary px-4 font-semibold rounded-md w-full'>
+                                            <Link to='/login'>Login</Link>
+                                        </button>
+                                        <button className=' btn-secondary px-4 font-semibold rounded-md w-full'>
+                                            <Link to='/signup'>Signup</Link>
+                                        </button>
+
+                                    </div>
+                                </li>
+                            )}
+
+                            {islogin &&
+                            (
+                                <li className=' absolute bottom-4 w-[90%]'>
+                                    <div className=' w-full flex items-center justify-center'>
+                                        <button className=' btn-primary px-4 font-semibold rounded-md w-full'>
+                                            <Link to='/user/profile'>Profile</Link>
+                                        </button>
+                                        <button className=' btn-secondary px-4 font-semibold rounded-md w-full'>
+                                            <Link onClick={handellogout} to='/logout'>Logout</Link>
+                                        </button>
+
+                                    </div>
+                                </li>
+                            )}
 
                     </ul>
                 </div>
 
             </div>
             {children}
-            <Footer/>
+            <Footer />
         </div>
     )
 }
